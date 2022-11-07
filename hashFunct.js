6445696a937e50777e16feee006b282bb7d206ea
Ramin <farhadiramin91@gmail.com>
2022-11-07 12:14:41 -0500

    Linked list was added
    In case of generating a same hash, a linkedlist will generate to prevents error. In this case Big O would be O(n).

--- hashFunct.js
+++ hashFunct.js
@@ -15,12 +15,23 @@ class HashTable {
 
   set(key, value) {
     let hash = this._hash(key);
-    this.data[hash] = value;
+    if (!this.data[hash]) {
+      this.data[hash] = [];
+    }
+    this.data[hash].push([key, value]);
   }
   get(key) {
     let hash = this._hash(key);
+    if (this.data[hash].length > 0) {
+      for (let i = 0; i < this.data[hash].length; i++) {
+        const currentBucket = this.data[hash];
+        if (currentBucket[i][0] == key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
     return this.data[hash];
   }
 }
 
-const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(5);
